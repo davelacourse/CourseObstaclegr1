baf1390d52ec1d13ae880a262d1a04c8cb27616a
ProfDave <dave.lacourse@cegeptr.qc.ca>
2024-03-25 17:39:19 -0400

    ui1

---
+++ Assets/_MesAssets/Scripts/UI/UIManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _panneauInstructions = default;
+    [SerializeField] private GameObject _panneauBoutons = default;
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+    public void ChangerScene()
+    {
+        int noScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(noScene + 1);
+    }
+
+    public void AfficherInstructions()
+    {
+        _panneauInstructions.SetActive(true);
+        _panneauBoutons.SetActive(false);
+    }
+
+    public void AfficherBoutons()
+    {
+        _panneauInstructions.SetActive(false);
+        _panneauBoutons.SetActive(true);
+    }
+
+    public void ChargerSceneDepart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
